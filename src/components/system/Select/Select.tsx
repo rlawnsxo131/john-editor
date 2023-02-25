b85b197edfe1cdae2840f43c6b893361467e7eef
juntae <rlawnsxo131@gmail.com>
2023-02-25 16:33:47 +0900

    add: select box 작성중

--- src/components/system/Select/Select.tsx
+++ src/components/system/Select/Select.tsx
@@ -1,10 +1,12 @@
 import SelectContent from './SelectContent';
 import SelectMain from './SelectMain';
+import SelectOption from './SelectOption';
 import SelectTrigger from './SelectTrigger';
 
 const Select = Object.assign(SelectMain, {
   Trigger: SelectTrigger,
   Content: SelectContent,
+  Option: SelectOption,
 });
 
 export default Select;
