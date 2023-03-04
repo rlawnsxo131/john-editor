8ba0fd61c0ebaa9166ba3961d7056a5aa8f1cb7a
juntae <rlawnsxo131@gmail.com>
2023-03-05 04:59:54 +0900

    add: fix typo, add comment

--- src/components/hoc/withSuspense.tsx
+++ src/components/hoc/withSuspense.tsx
@@ -1,6 +1,9 @@
 import type { ComponentType } from 'react';
 import { Suspense } from 'react';
 
+/**
+ * @TODO react router 에서 제공하는 <Await /> 이랑 쓰는게 나을지 직접 만들지 고민해보기
+ */
 export default function withSuspense<
   Props extends Record<string, unknown> = Record<string, never>,
 >(Component: ComponentType<Props>) {
