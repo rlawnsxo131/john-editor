f363d92d5f692f1bec451e93ec8bb9757c3719ff
juntae <rlawnsxo131@gmail.com>
2023-03-26 03:46:38 +0900

    comment: 주석 수정

--- src/lib/editor/Monaco.ts
+++ src/lib/editor/Monaco.ts
@@ -49,8 +49,8 @@ export default class Monaco {
   #onceCreateEditor(theme: Theme) {
     /**
      * @description monaco 생성시 HTMLElement 가 필요한데, react component 가 unmount 되었다가 다시 붙을때,
-     * @description 같은 HTMLElement 참조를 보지 않기때문에 미리 고정된 HTMLElement 를 만들어 놓고,
-     * @description 불필요한 class/editor 의 재생성을 회피합니다.
+     * 같은 HTMLElement 참조를 보지 않기때문에 미리 고정된 HTMLElement 를 만들어 놓고,
+     * 불필요한 class/editor 의 재생성을 회피합니다.
      */
     if (!this.#editorContainer) {
       const editorContainer = document.createElement('div');
