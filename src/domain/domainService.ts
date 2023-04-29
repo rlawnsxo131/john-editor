d0e5b6e8da69f425496753a0e8b6542c3f3f222d
juntae <rlawnsxo131@gmail.com>
2023-04-29 19:37:28 +0900

    refactor: 소소한 수정

--- src/domain/domainService.ts
+++ src/domain/domainService.ts
@@ -1,4 +1,4 @@
-import { repository } from '@/db';
+import { repository, SCHEMA_NAME_OBJECT } from '@/db';
 
 import { SUPPORT_LANGUAGES, SUPPORT_LAUNGUAGE_INITIAL_VALUE } from './models';
 
@@ -6,9 +6,12 @@ export async function initializeSupportLanguageRecords() {
   const result: IDBValidKey[] = [];
 
   for (const language of SUPPORT_LANGUAGES) {
-    const exists = await repository.getByKey('support_language', language);
+    const exists = await repository.getByKey(
+      SCHEMA_NAME_OBJECT.support_language,
+      language,
+    );
     if (!exists) {
-      const resKey = await repository.put('support_language', {
+      const resKey = await repository.put(SCHEMA_NAME_OBJECT.support_language, {
         language,
         origin: SUPPORT_LAUNGUAGE_INITIAL_VALUE[language].origin,
         modify: SUPPORT_LAUNGUAGE_INITIAL_VALUE[language].modify,
